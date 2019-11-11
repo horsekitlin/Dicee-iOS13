6388d6009ddfd72f2f50dcd062a7fd73c6b3f8f6
林暐秦（Tomas） <tomas.lin@velocitylight.net>
2019-11-11 19:08:28 +0800

    update dice image name

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,14 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var leftDiceImage: UIImageView!
 
-    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var rightDiceImage: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix");
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo");
+        leftDiceImage.image = #imageLiteral(resourceName: "DiceSix");
+        rightDiceImage.image = #imageLiteral(resourceName: "DiceTwo");
     }
 }
 
